734ed48f9762db1076aeeb0d0d7d26dfeba31f09
Fikri Hidayat <fikrihidayat799@gmail.com>
2023-11-16 15:38:46 +0700

    update 3

--- scriptgallery.js
+++ scriptgallery.js
@@ -6,11 +6,11 @@
 
 
 // You can change global variables here:
-var radius = 240; // how big of the radius
+var radius = 150; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
+var imgHeight = 140; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
 var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
